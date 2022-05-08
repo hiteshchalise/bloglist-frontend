04112f3ed345f29272b69c33a67528824fb3e88e
Hitesh Chalise <hiteshchalise10@gmail.com>
2022-05-08 18:48:31 +0545

    added loginService and implemented localstorage to store user credentials

--- src/App.js
+++ src/App.js
@@ -1,22 +1,94 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/loginService'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  useEffect(() => {
+    const loggedBlogUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedBlogUserJSON) {
+      const parsedUser = JSON.parse(loggedBlogUserJSON)
+      setUser(parsedUser)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+      setErrorMessage('wrong credentials')
+      setTimeout(() => setErrorMessage(''), 3000)
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="username"
+            onChange={(event) => setUsername(event.target.value)} />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+      {errorMessage}
+    </div>
+  )
+
+  const blogList = () => (
+    <div>
+      <h2>blogs</h2>
+      <div>
+        {user.name} logged in <button onClick={() => {
+          window.localStorage.removeItem('loggedBlogUser')
+          setUser(null)
+        }}>logout</button>
+      </div>
+      <br />
+      {
+        blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )
+      }
+    </div>
+  )
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null ? loginForm() : blogList()}
     </div>
   )
 }
